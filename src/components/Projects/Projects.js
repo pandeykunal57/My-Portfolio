b31e52dc87fc3cb5cd8a83907a40e301b45d76ae
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2025-06-29 12:34:11 +0530

    Projects And Resume Updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,11 @@ import notes from "../../Assets/Projects/notes.png";
 import travel from "../../Assets/Projects/travel.png";
 import login from "../../Assets/Projects/login.png";
 import drum from "../../Assets/Projects/drum.png";
+import emp from "../../Assets/Projects/emp.png";
+import finlytics from "../../Assets/Projects/finlytics.png";
+import explore from "../../Assets/Projects/explore.png";
+
+
 
 function Projects() {
   return (
@@ -21,6 +26,42 @@ function Projects() {
           Here are a few personal projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={finlytics}
+              isBlog={false}
+              title="FINLYTICS AI"
+              description="FINLYTICS AI – an intelligent finance platform for tracking income, expenses, and budgets. Features include real-time transaction updates, AI-powered receipt scanning, smart budget insights, and interactive financial reports. Helps users manage money smarter, reduce overspending, and achieve financial goals."
+              ghLink="https://github.com/pandeykunal57/FINLYTICS-AI"
+              demoLink="https://finlytics-ai.vercel.app/" 
+              
+            />
+          </Col>
+
+           <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emp}
+              isBlog={false}
+              title="Employee Task Management"
+              description="Developed a role-based Employee Task Management System using React.js with separate dashboards for Admin and Employees. Admins can assign and manage tasks, while employees can view and update their tasks. Used Context API for state management and `localStorage` for data persistence, enabling seamless login sessions and task handling without a backend."
+              ghLink="https://github.com/pandeykunal57/Employee-Task-Management"
+              demoLink="https://employee-task-management-chi.vercel.app/" 
+              
+            />
+          </Col>
+         
+           <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={explore}
+              isBlog={false}
+              title="ExploreX"
+              description="ExploreX is a React-based web app that displays a list of countries in a card format, showing details like capital, currency, and population. Users can click a card for more in-depth information. Features include continent-based filtering, a search bar, and sorting options, all working simultaneously for seamless exploration."
+              ghLink="https://github.com/pandeykunal57/ExploreX"
+              demoLink="https://explore-x-ten.vercel.app/" 
+              
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={food}
